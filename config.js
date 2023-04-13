60224c690952cc976aec66ad64e79e1a0d19b76b
gabrielaclb <79880262+gabrielaclb@users.noreply.github.com>
2023-04-13 02:01:41 -0400

    Backend2

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB_URL:
+    "mongodb://mongo:quKreKwssWHhlLVgUwBB@containers-us-west-139.railway.app:5856",
+  JWT_SECRET: "eyJhbGciOiJIUzIMzJtAgyTfMKqh7F-V8shxwKP70eZdxznkZSzss",
+};
